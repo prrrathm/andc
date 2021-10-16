4f510a0faf68519a4ffe277d221e7a3c1987bdd4
prachand4 <prachand4@publicissapient.com>
2021-10-16 09:48:48 +0530

    helloworld using react js

---
+++ daythree/helloworld/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const name = 'George Bush';
+//const element = <h1>Hello, {name}</h1>;
+  
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
