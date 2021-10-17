a8c2c202da1a2253a572facd9235a06551215832
prachand4 <prachand4@publicissapient.com>
2021-10-17 11:04:55 +0530

    Updated code for board and square.js

--- dayfour/my-app/src/component/board.js
+++ dayfour/my-app/src/component/board.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Square from './square';
+/**
+ * ['o',null,'x',
+ *  'x','x','o',
+ *  'o',null,null
+ * ]
+ * 
+ */
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      squares:Array(9).fill(null)
+    };
+}
   renderSquare(i) {
-    return <Square value={i} />;
+   // return <Square value={i} />;
+   return <Square value={
+     this.state.squares[i]} onClick={
+       ()=> this.handleClick(i)} />;
+  }
+
+  handleClick(i){
+    const squares=this.state.squares.slice();
+    squares[i]='X';
+    this.setState({squares: squares});
   }
 
   render() {
--- dayfour/my-app/src/component/square.js
+++ dayfour/my-app/src/component/square.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor(props){
+      super(props);
+      this.state={
+        value:null
+      };
+  }
+
   render() {
     return (
-      <button className="square">
+      /**
+      <button className="square" onClick={function(){console.log('click');}}>
         {this.props.value}
-      </button>
+      </button> */
+      /**
+      <button className="square" onClick={()=>{console.log('click')}}>
+      {this.props.value}
+    </button>
+     */
+    /**
+    <button className="square" onClick={
+      ()=>{this.setState({value: 'X'})}
+      }>
+      {this.state.value}
+    </button>
+     */
+    <button className="square" onClick={
+      ()=>{this.props.onClick()}
+      }>
+      {this.props.value}
+    </button>
     );
   }
 }
