d99cbfa88ccb1b9670a9210c5d1e2c8d268e5e94
prachand4 <prachand4@publicissapient.com>
2021-10-17 09:56:49 +0530

    day four exercise and drag and drop app

---
+++ dayfour/my-app/src/component/square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+
+export default Square;
